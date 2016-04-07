5266f1a78fff9fbfc6e73275e1fe8e7c6d08b4d1
JcPires <djyss@live.fr>
2016-04-07 17:03:22 +0200

    Some fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,15 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+js_prefix    = 'app/assets/javascripts/'
+style_prefix = 'app/assets/stylesheets/'
+
+javascripts = Dir["#{js_prefix}**/*.js"].map      { |x| x.gsub(js_prefix,    '') }
+css         = Dir["#{style_prefix}**/*.css"].map  { |x| x.gsub(style_prefix, '') }
+scss        = Dir["#{style_prefix}**/*.scss"].map { |x| x.gsub(style_prefix, '') }
+
+Rails.application.config.assets.precompile = (javascripts + css + scss)
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
