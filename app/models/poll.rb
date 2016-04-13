cf240f9adaac143c46022af8029b2dfab16ba4dd
Jemoms <morej03@gmail.com>
2016-04-13 15:24:22 +0200

    modif poll op cheat

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,6 +1,4 @@
 class Poll < ActiveRecord::Base
-  has_many :fields, class_name: "PollsField",
-                        foreign_key: "polls_field_id"
-  has_many :responses, class_name: "PollsFieldsResponse",
-                        foreign_key: "polls_fields_response_id"
+  has_many :fields, class_name: "PollsField"
+  has_many :responses, class_name: "PollsFieldsResponse"
 end
--- app/models/polls_field.rb
+++ app/models/polls_field.rb
@@ -4,4 +4,17 @@ class PollsField < ActiveRecord::Base
                         foreign_key: "poll_id"
   has_many :responses, class_name: "PollsFieldsResponse",
                         foreign_key: "polls_fields_response_id"
+
+  def optionsFormated
+    if self.options != nil
+      return self.options.split(",").map(&:to_s)
+    end
+  end
+
+  def rulesFormated
+    if self.rules != nil
+      return self.rules.remove(",")
+    end
+  end
+  
 end
