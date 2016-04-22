e3b0ee63e8a0ff64a9890fc06b2d0d27b89cb1b2
JcPires <djyss@live.fr>
2016-04-22 10:20:18 +0200

    Notifications systeme v0.2

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,14 @@ class Notification < ActiveRecord::Base
   belongs_to :receiver, class_name: "User", foreign_key: "receiver"
   belongs_to :sender, class_name: "User", foreign_key: "sender"
 
+  def getRelationData
+    @data = eval(self.model).find( self.key )
+    
+    if ( self.model == "BlogArticle" )
+      return @data.title
+    elsif ( self.model == "Project")
+      return @data.name
+    end
+  end
+
 end
